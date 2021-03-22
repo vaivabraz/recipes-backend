3eadb737eb65791cf375396d73554fc6a13d5de8
vabr@danskebank.lt <vabr@danskebank.lt>
2021-03-22 10:37:08 +0200

    db connect logic

---
+++ src/connectDB.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const mongooseConnect = (db: string) => {
+  const connect = () => {
+    mongoose
+      .connect(db, { useNewUrlParser: true })
+      .then(() => {
+        return console.log(`Successfully connected to ${db}`);
+      })
+      .catch((error) => {
+        console.log('Error connecting to database: ', error);
+        return process.exit(1);
+      });
+  };
+  connect();
+
+  mongoose.connection.on('disconnected', connect);
+};
+
+export default mongooseConnect;
