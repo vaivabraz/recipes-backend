f9dc327c2aa080a5d114e27ffb294d80ae063931
Vaiva <vaivabraz@gmail.com>
2022-07-18 15:52:15 +0300

    categories filter

--- src/controllers/recipes/GetRecipes.controller.ts
+++ src/controllers/recipes/GetRecipes.controller.ts
@@ -3,7 +3,18 @@ import { RecipeModel } from '../../models';
 
 export const getMyRecipes = async (req: Request, res: Response) => {
   try {
-    const userRecipes = await RecipeModel.where('author')
+    if (req.query.categories){
+      const userRecipes = await RecipeModel
+      .where('author')
+      .equals(req.body.payload.username)
+      .where('categories.id').equals(req.query.categories)
+      .sort({ _id: -1 })
+      .limit(20);
+
+    return res.send(userRecipes);
+    }
+    const userRecipes = await RecipeModel
+      .where('author')
       .equals(req.body.payload.username)
       .sort({ _id: -1 })
       .limit(20);
